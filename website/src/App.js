bb3adff7e1224c144f996fb6fc38b0fc2854f9a0
Jake-Bodin <jakebodin@gmail.com>
2023-06-13 13:46:27 -0400

    Update App.js

--- website/src/App.js
+++ website/src/App.js
@@ -5,7 +5,7 @@ const Header = styled.header`
   display: flex;
   justify-content: space-between;
   padding: 10px;
-  background-color: #282c34;
+  background-color: #282c34 !important; /* Added !important */
   color: white;
 `;
 
@@ -54,17 +54,17 @@ const ProjectDescription = styled.p`
 const Footer = styled.footer`
   padding: 10px;
   text-align: center;
-  background-color: #282c34;
+  background-color: #282c34 !important; /* Added !important */
   color: white;
 `;
 
 const HomePage = () => {
-  const randomImage = "https://unsplash.com/photos/rCbdp8VCYhQ"; // fetches a image from Unsplash
+  const randomImage = "https://images.unsplash.com/photo-1528722828814-77b9b83aafb2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"; // fetches an image from Unsplash
 
   return (
     <div>
       <Header>
-        <HeaderName>Your Name</HeaderName>
+        <HeaderName>Jake Bodin</HeaderName>
         <Navigation>
           <NavButton>Home</NavButton>
           <NavButton>About Me</NavButton>
@@ -73,25 +73,25 @@ const HomePage = () => {
         </Navigation>
       </Header>
 
-      <MainImage src={randomImage} alt="Random"/>
+      <MainImage src={randomImage} alt="Random" />
 
       <ProjectPreview>
         <ProjectCard>
-          <ProjectImage src="https://source.unsplash.com/random?coding" alt="Project 1"/>
+          <ProjectImage src="https://source.unsplash.com/random?coding" alt="Project 1" />
           <ProjectDescription>Project 1: A short description of the project goes here.</ProjectDescription>
         </ProjectCard>
         <ProjectCard>
-          <ProjectImage src="https://source.unsplash.com/random?coding" alt="Project 2"/>
+          <ProjectImage src="https://source.unsplash.com/random?coding" alt="Project 2" />
           <ProjectDescription>Project 2: A short description of the project goes here.</ProjectDescription>
         </ProjectCard>
         <ProjectCard>
-          <ProjectImage src="https://source.unsplash.com/random?coding" alt="Project 3"/>
+          <ProjectImage src="https://source.unsplash.com/random?coding" alt="Project 3" />
           <ProjectDescription>Project 3: A short description of the project goes here.</ProjectDescription>
         </ProjectCard>
       </ProjectPreview>
 
       <Footer>
-        Â© {new Date().getFullYear()} Your Name. All rights reserved.
+        © {new Date().getFullYear()} Your Name. All rights reserved.
       </Footer>
     </div>
   );
